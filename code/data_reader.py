5223c154561bf926f0bb0c6f911907ce8f024a70
Charlotte Lambert <clambert@vassar.edu>
2019-09-27 09:01:47 -0400

    Handle errors.

--- code/data_reader.py
+++ code/data_reader.py
@@ -7,7 +7,12 @@ from tei_reader import TeiReader
 
 def main():
     # Load data directory and pick output file
-    XML_data_dir = sys.argv[1]
+    try:
+        XML_data_dir = sys.argv[1]
+    except:
+        print("Input directory of XML files")
+        sys.exit(1)
+
     input_files = [os.path.join(XML_data_dir, f) for f in os.listdir(XML_data_dir) if os.path.isfile(os.path.join(XML_data_dir, f))]
     txt_output_dir = os.path.dirname(XML_data_dir) + "-txt"
     if not os.path.exists(txt_output_dir):
@@ -18,17 +23,24 @@ def main():
 
     # Go through input files and generate output files
     for i in tqdm(range(len(input_files))):
-        # Get text from tei_reader
+        # Get current file
         file = input_files[i]
-        text = reader.read_file(file)
 
         # Change to txt file
         filename = os.path.splitext(os.path.basename(file))[0] + ".txt"
+        file_path = os.path.join(txt_output_dir, filename)
+        if os.path.exists(file_path):
+            continue
+
         # Write text to txt file
         with open(os.path.join(txt_output_dir, filename), "w+") as f:
+            # Get text from tei_reader
+            try:
+                text = reader.read_file(file)
+            except UnicodeDecodeError:
+                print("Error reading " + file + ". Skipping...")
+                continue
             f.write(text.text)
-
-
 
 
 if __name__ == '__main__':
