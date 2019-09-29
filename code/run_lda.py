f6e53330076915113924ecd90df90573bb5dfc10
Charlotte Lambert <clambert@vassar.edu>
2019-09-29 16:19:28 -0400

    Change model saving.

--- code/run_lda.py
+++ code/run_lda.py
@@ -22,11 +22,6 @@ from gensim.models.phrases import Phrases, Phraser
 # #SBATCH -c 64
 # export JAVA_OPTIONS="-Xms4G -Xmx8G"
 
-# Where is Mallet installed? Can be overridden with MALLET_PATH environment
-# variable, e.g.,
-#     MALLET_PATH=~/opt/mallet/bin/mallet ./run_lda.py
-# MALLET_PATH = os.environ.get("MALLET_PATH", "~/Mallet/bin/mallet")
-
 def print_params(pre, args):
     # Print out arguments used to file
     with open(pre + "parameters.tsv", "w+") as f:
@@ -35,11 +30,35 @@ def print_params(pre, args):
         for key in arg_dict:
             tsv_writer.writerow([key, arg_dict[key]])
 
+def get_ngrams(args, texts):
+    if not args.unigrams_only:
+        print("Finding bigrams.", file=sys.stderr)
+        bigram = Phrases(texts, min_count=1) # Is this an appropriate value for min_count?
+        bigram = Phraser(bigram)
+        for idx in tqdm(range(len(texts))):
+            bigrams = bigram[texts[idx]]
+            if args.bigrams_only:
+                texts[idx] = [] # If we only want bigrams, remove all unigrams
+            for token in bigrams:
+                if '_' in token:
+                    texts[idx].append(token)
+                    if args.mixed_ngrams:
+                        texts[idx].append(token) # Scale bigrams
+    return texts
+
 def LDA_on_directory(args):
-    if args.lda_type == "multicore":
-        lda = gensim.models.ldamulticore.LdaMulticore
-    elif args.lda_type == "mallet":
-        lda = gensim.models.wrappers.LdaMallet
+    # Types of valid lda models to run
+    type_list = ["mallet", "multicore", "dtm"]
+    if args.lda_type not in type_list:
+        print("Please specify a valid model type (multicore, mallet, dtm).", sys.err)
+        sys.exit(1)
+
+    # Prefix for running lda (modify if files should go to a different directory)
+    # FORMAT: "../models/lda_type/YYYYMMDD/HH-MM-SS"
+    pre = args.save_model_dir + args.lda_type + "/" + time.strftime("%Y%m%d") + "/" + time.strftime("%H-%M-%S") + "/"
+
+    if not os.path.exists(pre):
+        os.mkdir(pre)
 
     print("Reading corpus.", file=sys.stderr)
 
@@ -60,19 +79,7 @@ def LDA_on_directory(args):
             texts.append(text)
 
     # If we want to include a mix of unigrams and bigrams or just bigrams
-    if not args.unigrams_only:
-        print("Finding bigrams.", file=sys.stderr)
-        bigram = Phrases(texts, min_count=1) # Is this an appropriate value for min_count?
-        bigram = Phraser(bigram)
-        for idx in tqdm(range(len(texts))):
-            bigrams = bigram[texts[idx]]
-            if args.bigrams_only:
-                texts[idx] = [] # If we only want bigrams, remove all unigrams
-            for token in bigrams:
-                if '_' in token:
-                    texts[idx].append(token)
-                    if args.mixed_ngrams:
-                        texts[idx].append(token) # Scale bigrams
+    texts = get_ngrams(args, texts)
 
     print("Building dictionary.", file=sys.stderr)
 
@@ -83,22 +90,28 @@ def LDA_on_directory(args):
 
     corpus = [dictionary.doc2bow(text) for text in texts]
 
-    if not os.path.exists(args.save_model_dir):
-        os.mkdir(args.save_model_dir)
-
-    # Prefix for running lda (modify if files should go to a different directory)
-    pre = args.save_model_dir + "/" + time.strftime("%H:%M:%S") + "_" + args.lda_type + "."
-
-    # Run specified model
-    if lda == gensim.models.wrappers.LdaMallet:
-        ldamodel = lda(args.mallet_path, corpus, num_topics=args.num_topics,
-                       id2word=dictionary, optimize_interval=args.optimize_interval,
-                       workers=12, iterations=args.num_iterations,
-                       prefix=pre)
-    elif lda == gensim.models.ldamulticore.LdaMulticore:
+    # Run the specified model
+    if args.lda_type == "multicore":
+        lda = gensim.models.ldamulticore.LdaMulticore
         ldamodel = lda(corpus, num_topics=args.num_topics,
                        id2word=dictionary, passes=200, alpha=20, workers=8,
                        prefix=pre)
+    elif args.lda_type == "mallet":
+        MALLET_PATH = os.environ.get("MALLET_PATH", "~/Mallet/bin/mallet")
+        lda = gensim.models.wrappers.LdaMallet
+        ldamodel = lda(MALLET_PATH, corpus, num_topics=args.num_topics,
+                       id2word=dictionary, optimize_interval=args.optimize_interval,
+                       workers=12, iterations=args.num_iterations,
+                       prefix=pre)
+    elif args.lda_type == "dtm": # Dynamic Topic Model
+        lda = gensim.models.wrappers.DtmModel
+        # Find path to DTM binary
+        DTM_PATH = os.environ.get('DTM_PATH', None)
+        if not DTM_PATH:
+            raise ValueError("You need to set the DTM path")
+        # Run the model
+        ldamodel = lda(DTM_PATH, corpus=corpus,
+            id2word=dictionary, time_slices=[1] * len(corpus), prefix=pre)
 
     # Save model with timestamp
     ldamodel.save(pre + "model")
@@ -122,8 +135,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mallet_path', type=str, default=os.environ.get("MALLET_PATH", "~/Mallet/bin/mallet"), help='path to where mallet is installed')
-    parser.add_argument('--save_model_dir', type=str, default="../models/" + time.strftime("%Y%m%d"), help='path for saving model')
+    parser.add_argument('--save_model_dir', type=str, default="../models/", help='base directory for saving model directory')
     parser.add_argument('--unigrams_only', default=False, action="store_true", help='whether or not to only include unigrams')
     parser.add_argument('--bigrams_only', default=False, action="store_true", help='whether or not to only include bigrams')
     parser.add_argument('--mixed_ngrams', default=False, action="store_true", help='whether or not to include both unigrams and bigrams')
