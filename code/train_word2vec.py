3acee31dc9331e42405a61340b50d81dd477c043
Charlotte Lambert <clambert@vassar.edu>
2019-11-17 21:56:27 -0500

    Filter out top words.

--- code/train_word2vec.py
+++ code/train_word2vec.py
@@ -5,6 +5,7 @@ from gensim.models import Word2Vec
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import numpy as np
 
 def build_corpus(input_dir):
     files = [os.path.join(input_dir, f) for f in os.listdir(input_dir)
@@ -14,6 +15,22 @@ def build_corpus(input_dir):
         with open(file) as f:
             corpus.append(f.read().split())
     return corpus
+
+# https://stackoverflow.com/questions/48941648/how-to-remove-a-word-completely-from-a-word2vec-model-in-gensim
+def filter_top_words(model, n):
+    wv = model.wv
+    words_to_trim = wv.index2word[n:]
+    ids_to_trim = [wv.vocab[w].index for w in words_to_trim]
+
+    for w in words_to_trim:
+        del wv.vocab[w]
+
+    wv.vectors = np.delete(wv.vectors, ids_to_trim, axis=0)
+    wv.init_sims(replace=True)
+
+    for i in sorted(ids_to_trim, reverse=True):
+        del(wv.index2word[i])
+    return model
 
 # https://www.kaggle.com/jeffd23/visualizing-word-vectors-with-t-sne/notebook
 def tsne_plot(model, pre):
@@ -56,6 +73,7 @@ def tsne_plot(model, pre):
     print("TSNE plot saved.", file=sys.stderr)
 
 def main(args):
+    print("Beginning at " + time.strftime("%d/%m/%Y %H:%M "), file=sys.stderr)
     if not  args.load_model:
         pre = args.save_model_dir + "word2vec/" + time.strftime("%Y-%m-%d") + "/" + time.strftime("%H-%M-%S") + "/"
         if not os.path.exists(pre):
@@ -67,8 +85,14 @@ def main(args):
         model = Word2Vec(min_count=1)#, size=100, window=20)#, workers=4)
         print("Building vocab...", file=sys.stderr)
         model.build_vocab(corpus)
+
+        # Filter out top words (need to filter to 10000 if using projector.tensorflow)
+        if args.filter_top_words:
+            print("Extracting top " + str(args.filter_top_words) + " words...", file=sys.stderr)
+            model = filter_top_words(model, args.filter_top_words)
         print("Training model...", file=sys.stderr)
         model.train(corpus, total_examples=model.corpus_count, epochs=model.epochs)
+
         model.save(pre + "model")
         print("Model saved. ", file=sys.stderr)
     else:
@@ -81,15 +105,16 @@ def main(args):
         print("Visualizing results...", file=sys.stderr)
         tsne_plot(model, pre)
 
-    print("Done!", file=sys.stderr)
+    print("Done! Ending at " + time.strftime("%d/%m/%Y %H:%M ") , file=sys.stderr)
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--corpus_dir', type=str, default="/work/clambert/thesis-data/sessionsAndOrdinarys-txt-tok-dh", help='directory containing corpus')
-    parser.add_argument('--save_model_dir', type=str, default="../models/", help='base directory for saving model directory')
+    parser.add_argument('--save_model_dir', type=str, default="/work/clambert/models/", help='base directory for saving model directory')
     parser.add_argument('--load_model', type=str, help='path to model to load and visualize.')
     parser.add_argument('--no_plot', default=False, action="store_true", help='whether or not to visualize and plot data.')
+    parser.add_argument('--filter_top_words', type=int, default=10000, help='number of words to include in model (take the most common words)')
     args = parser.parse_args()
     main(args)
