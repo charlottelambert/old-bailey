aa6fefca8f9a03965c12df04764acb8eeef0e1d5
Charlotte Lambert <clambert@vassar.edu>
2019-12-12 16:41:01 -0500

    Add nearest neighbors calculation.

--- code/train_word2vec.py
+++ code/train_word2vec.py
@@ -1,23 +1,57 @@
 #!/usr/bin/env python3
 
-import os, sys, argparse, time
+import os, sys, argparse, time, csv
 from gensim.models import Word2Vec
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import numpy as np
+from utils import *
 
-def build_corpus(input_dir):
-    files = [os.path.join(input_dir, f) for f in os.listdir(input_dir)
-                 if (os.path.isfile(os.path.join(input_dir, f)) and f.endswith('.txt'))]
+word_list = ["punishment", "guilt", "murder", "suffrage", "woman", "man", "innocent"]
+
+# want to do this once for all models trained
+def find_n_neighbors(args, pre, model_dict):
+    print(timestamp(), "Finding nearest", args.find_n_neighbors, "neighbors...", file=sys.stderr)
+    tsv_path = os.path.join(pre, "neighbors.tsv")
+    with open(tsv_path, "w") as f:
+        tsv_writer = csv.writer(f, delimiter='\t')
+        tsv_writer.writerow(["word", "year", "neighbors", "model path"])
+        for first_year, model_info in model_dict.items():
+            for word in word_list:
+                try:
+                    neighbor_list = [neighbor[0] for neighbor in model_info["model"].similar_by_word(word, args.find_n_neighbors)]
+                    tsv_writer.writerow([word, first_year, ", ".join(neighbor_list), model_info["model_path"]])
+                except KeyError:
+                    print("Word \"" + word + "\" not in vocabulary. Skipping...", file=sys.stderr)
+
+    print(timestamp() + " Wrote top", args.find_n_neighbors, "neighbors to", tsv_path, file=sys.stderr)
+
+
+def build_corpus(input_dir_path=None, files=None):
+    """
+        Function to build a corpus (list of contents of files) based on input
+        directory.
+    """
+    if not files and input_dir_path:
+        files = [os.path.join(input_dir, f) for f in os.listdir(input_dir)
+                     if (os.path.isfile(os.path.join(input_dir, f)) and f.endswith('.txt'))]
+    elif not input_dir_path and not files:
+        print("build_corpus(): Please input a path to a directory or a list of files.", file=sys.stderr)
+        exit(1)
     corpus = []
     for file in files:
         with open(file) as f:
             corpus.append(f.read().split())
     return corpus
 
+# want to do this for each model trained
 # https://stackoverflow.com/questions/48941648/how-to-remove-a-word-completely-from-a-word2vec-model-in-gensim
 def filter_top_words(model, n):
+    """
+        Filter the most frequent n words found by model. Maximum of 10,000 words
+        can be visualized using projector.tensorflow.org.
+    """
     wv = model.wv
     words_to_trim = wv.index2word[n:]
     ids_to_trim = [wv.vocab[w].index for w in words_to_trim]
@@ -32,9 +66,13 @@ def filter_top_words(model, n):
         del(wv.index2word[i])
     return model
 
+# want to do this for each model trained
 # https://www.kaggle.com/jeffd23/visualizing-word-vectors-with-t-sne/notebook
 def tsne_plot(model, pre):
-    "Creates and TSNE model and plots it"
+    """
+        Creates and TSNE model and plots it
+    """
+
     labels = []
     tokens = []
     for i, word in enumerate(model.wv.vocab):
@@ -43,12 +81,12 @@ def tsne_plot(model, pre):
 
 #    tsne_model = TSNE(perplexity=30, n_components=2, init='pca', n_iter=2500, random_state=23)
     tsne_model = TSNE(random_state=2017, perplexity=12, n_components=2, init='pca', method='barnes_hut', verbose=1)
-    print("TSNE model initialized.", file=sys.stderr)
+    print(timestamp(), "TSNE model initialized.", file=sys.stderr)
     # PROBLEM with fit_transform, it's just never returning
     try:
         new_values = tsne_model.fit_transform(tokens)
     except KeyboardInterrupt:
-        print("Exiting...", file=sys.stderr)
+        print(timestamp(), "Exiting...", file=sys.stderr)
 
     x = []
     y = []
@@ -70,42 +108,55 @@ def tsne_plot(model, pre):
                      va='bottom')
     # plt.show()
     plt.savefig(os.path.join(pre, 'plot.png'))
-    print("TSNE plot saved.", file=sys.stderr)
+    print(timestamp(), "TSNE plot saved.", file=sys.stderr)
 
 def main(args):
-    print("Beginning at " + time.strftime("%d/%m/%Y %H:%M "), file=sys.stderr)
-    if not  args.load_model:
+    print(timestamp(), "Beginning at " + time.strftime("%d/%m/%Y %H:%M "), file=sys.stderr)
+    if not args.load_model:
         pre = args.save_model_dir + "word2vec/" + time.strftime("%Y-%m-%d") + "/" + time.strftime("%H-%M-%S") + "/"
         if not os.path.exists(pre):
             os.makedirs(pre)
-        print("Data will be saved to directory " + pre, file=sys.stderr)
-        print("Building corpus...", file=sys.stderr)
-        corpus = build_corpus(args.corpus_dir)
 
-        model = Word2Vec(min_count=1)#, size=100, window=20)#, workers=4)
-        print("Building vocab...", file=sys.stderr)
-        model.build_vocab(corpus)
+        # Order files by year
+        files_dict = order_files(args)
 
-        # Filter out top words (need to filter to 10000 if using projector.tensorflow)
-        if args.filter_top_words:
-            print("Extracting top " + str(args.filter_top_words) + " words...", file=sys.stderr)
-            model = filter_top_words(model, args.filter_top_words)
-        print("Training model...", file=sys.stderr)
-        model.train(corpus, total_examples=model.corpus_count, epochs=model.epochs)
+        print(timestamp(), "Data will be saved to directory " + pre, file=sys.stderr)
+        model_dict = {}
+        for first_year, file_list in files_dict.items():
+            print(timestamp(), "Building corpus...", file=sys.stderr)
+            corpus = build_corpus(files=file_list)
 
-        model.save(pre + "model")
-        print("Model saved. ", file=sys.stderr)
-    else:
-        # Load model from args.load_model
+            model = Word2Vec(min_count=1)#, size=100, window=20)#, workers=4)
+            print(timestamp(), "Building vocab...", file=sys.stderr)
+            model.build_vocab(corpus)
+
+            # Filter out top words (need to filter to 10000 if using projector.tensorflow)
+            if args.filter_top_words:
+                print(timestamp(), "Extracting top " + str(args.filter_top_words) + " words...", file=sys.stderr)
+                model = filter_top_words(model, args.filter_top_words)
+            print(timestamp(), "Training model...", file=sys.stderr)
+            model.train(corpus, total_examples=model.corpus_count, epochs=model.epochs)
+            model_path = os.path.join(pre, "model-" + str(first_year))
+            model.save(model_path)
+            model_dict[first_year] = {"model": model, "model_path": model_path}
+            print(timestamp(), "Model saved to", model_path, file=sys.stderr)
+
+    else: # FIX THIS SO IT CAN CALL FIND N NEIGHBORS PROPERLY
+        # Load model from args.load_model MAKE THIS LAOD A MODEL DIR INSTEAD OF A MODEL!
         model = Word2Vec.load(args.load_model)
-        print("Model loaded from " + args.load_model, file=sys.stderr)
+        print(timestamp(), "Model loaded from " + args.load_model, file=sys.stderr)
         pre = os.path.dirname(args.load_model)
 
+    if args.find_n_neighbors:
+        model_path = args.load_model if args.load_model else pre + "model"
+        find_n_neighbors(args, pre, model_dict)
+
+    # THIS IS BROKEN
     if args.plot:
-        print("Visualizing results...", file=sys.stderr)
+        print(timestamp(), "Visualizing results...", file=sys.stderr)
         tsne_plot(model, pre)
 
-    print("Done! Ending at " + time.strftime("%d/%m/%Y %H:%M ") , file=sys.stderr)
+    print(timestamp(), "Done! Ending at " + time.strftime("%d/%m/%Y %H:%M ") , file=sys.stderr)
 
 
 
@@ -116,5 +167,7 @@ if __name__ == '__main__':
     parser.add_argument('--load_model', type=str, help='path to model to load and visualize.')
     parser.add_argument('--plot', default=False, action="store_true", help='whether or not to visualize and plot data.')
     parser.add_argument('--filter_top_words', type=int, default=10000, help='number of words to include in model (take the most common words)')
+    parser.add_argument('--find_n_neighbors', type=int, default=0, help='how many nearest neighbors to find')
+    parser.add_argument('--year_split', type=int, default=100, help='number of years to include in each chunk of corpus (run tf-idf over each chunk)')
     args = parser.parse_args()
     main(args)
