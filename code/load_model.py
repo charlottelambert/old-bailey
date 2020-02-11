db9b80f50a74d1a7e398d998357da809e7ef8d60
Charlotte Lambert <clambert@vassar.edu>
2020-02-10 20:18:57 -0500

    Show results of DTM.

--- code/load_model.py
+++ code/load_model.py
@@ -5,30 +5,40 @@ from gensim.models.wrappers.dtmmodel import DtmModel
 
 def main(args):
     f = sys.argv[1]
-    #f.seek(0)
     if args.lda_type == "mallet":
         loaded_model = LdaModel.load(args.model)
+
+        for topic_num, topic in enumerate(loaded_model.show_topics(num_topics=-1)):
+
+            if args.lda_type == "mallet":
+                topic_num, topic_str = topic
+
+            print(str(topic_num) + ':', end=' ')
+            for term in topic_str.split(' + '):
+                weight, word = term.split('*')
+                if args.lda_type == "dtm":
+                    word = "\"" + word + "\""
+                print(word, end=' ')
+            print()
+
     elif args.lda_type == "dtm":
         loaded_model = DtmModel.load(args.model)
+        # maybe use dtm_coherence?
+        for topic_id in range(loaded_model.num_topics):
+            for time in range(len(loaded_model.time_slices)):
+                top_words = loaded_model.show_topic(topic_id, time, topn=10)
+
+                print("Topic", str(topic_id) + ", time slice", str(time) + ':', end=' ')
+                for weight, word in top_words:
+                    print(word, end=' ')
+                print()
+            print()
     else:
         print("Unknown lda_type provided: " + args.lda_type)
         sys.exit(1)
 
-    for topic_num, topic in enumerate(loaded_model.show_topics(num_topics=-1)):
-
-        if args.lda_type == "mallet":
-            topic_num, topic_str = topic
-        elif args.lda_type == "dtm":
-            topic_str = topic
 
 
-        print(str(topic_num) + ':', end=' ')
-        for term in topic_str.split(' + '):
-            weight, word = term.split('*')
-            if args.lda_type == "dtm":
-                word = "\"" + word + "\""
-            print(word, end=' ')
-        print()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
