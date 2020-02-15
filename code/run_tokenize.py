7395e068a94235bb2ee0ca7aa4e67bf04547baf9
Charlotte Lambert <clambert@vassar.edu>
2020-02-14 22:27:46 -0500

    Add spell-checking.

--- code/run_tokenize.py
+++ code/run_tokenize.py
@@ -9,12 +9,12 @@
 #
 ###############################################################################
 
-import sys, argparse, os, re
+import sys, argparse, os, re, enchant
 from tqdm import tqdm
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.util import ngrams
-from utils import c_dict
+from utils import *
 
 def contractions(token_list):
     # Remove all asterisks and replace contractions
@@ -51,10 +51,135 @@ def fix_hyphens(input):
 
     return out.split()
 
+def tokenize_file(args, file, output_dir, d):
+    output = []
+    with open(file, "r") as f:
+        for line in f:
+            if not line.strip():
+                continue
+
+            # Lower line if needed
+            if args.lower and not args.stats:
+                line = line.lower()
+
+            # Fix apostrophe escapes:
+            line = line.replace("\\\'", "'")
+
+            # Tokenize line
+            tokens = word_tokenize(line)
+
+            # Join $ to names
+            for i, tok in enumerate(tokens):
+                if tok == '$':
+                    tokens[i:i+2] = [''.join(tokens[i:i+2])]
+
+            # Spelling correction
+            updated_tokens = []
+            for i, tok in enumerate(tokens):
+                updated_tokens += spell_correct(args, d, tok).split()
+            tokens = updated_tokens
+
+            if args.bigrams and not args.stats:
+                tokens = make_bigrams(tokens)
+                output.append(" ".join(tokens))
+                continue
+
+            # Handle issue with dashes appearing at start of word
+            mod_tokens = []
+            for i in range(len(tokens)):
+                mod_tokens += tokens[i].split() #fix_hyphens(tokens[i])
+            tokens = mod_tokens
+
+
+            if not args.stats:
+                # First, remove trailing hyphens and slashes
+                # dash_pattern = r'([^‒–—―\-\\]*)([‒–—―\-\\]+)$' # FIX THIS: ITS REMOVING ALL TRAILING PUNCT
+                # tokens = [re.sub(dash_pattern, '\\1', x) for x in tokens]
+                sub_pattern = '\A([\W_]*)([A-Za-z0-9]+|[A-Za-z0-9]+[\W_]+[A-Za-z0-9]+)([\W_]*)$'
+                tokens = [re.sub(sub_pattern, "\\2", x) for x in tokens]
+                # Keep all words containing at least one letter
+                # Also remove words of length < 2
+                # Lemmatize if necessary
+                if args.lemma:
+                    tokens = [lemmatizer.lemmatize(x) for x in tokens if len(x) > 2 and re.search('[a-zA-Z]', x)]
+                else:
+                    tokens = [x for x in tokens if len(x) > 2 and re.search('[a-zA-Z]', x)]
+
+            # Replace split contractions with full words
+            tokens = contractions(tokens)
+
+            # If tokenizing text in order to find useful stats, do extra
+            # processing and return without removing words
+            if args.stats:
+                output.append(" ".join(tokens))
+                continue
+
+            finished = " ".join(tokens)
+
+            # If needed, replace street names with generic version
+            if args.street_sub:
+                finished = re.sub("([^ ]+\-street)|([A-Z][a-z]* street)", "$name_street", finished)
+                # finished = re.sub("([^ ]+\-lane)|([A-Z][a-z]* lane)", "$name_street", finished)
+                # finished = re.sub("([^ ]+\-road)|([A-Z][a-z]* road)", "$name_street", finished)
+                # finished = re.sub("[^ ]+\-row", "$name_street", finished)
+                # also -square, -highway, -cross, -grove, -town
+
+            output.append(finished)
+    return output
+
+# Eventually, we jsut want to run this on each word, might need a way to speed
+# this up, parallel?
+# gonna have millions of tokens, need to be more efficient!
+def spell_correct(args, d, word):
+    # print(word)
+    # If the line is a valid word, continue
+    if d.check(word):
+        return word
+
+    # Split word by non-alphanumeric characters
+    split_word = re.split("\W", word)
+
+    split_word = [w for w in split_word if not w == '']
+    corrected_word = split_word
+
+    for i, sub_word in enumerate(split_word):
+        # If sub_word is valid, don't change anything
+        if d.check(sub_word):
+            continue
+        else:
+            # Suggest corrections for sub_line
+            suggestions = d.suggest(sub_word)
+            # See if any of them are reasonable
+            # TODO: WANT THE MOST PROBABLE SUGGESTIONS! "houses i" vs "houses si"!
+            for opt in suggestions:
+                l = opt.split()
+                if len(l) == 2 and "".join(l) == sub_word:
+                    corrected_word[i] = opt
+                    break
+    return " ".join(corrected_word)
+
 def main(args):
-    # Compile list of files to tokenize
-    files = [os.path.join(args.corpus_dir, f) for f in os.listdir(args.corpus_dir)
-             if (os.path.isfile(os.path.join(args.corpus_dir, f)) and f.endswith('.txt'))]
+    if args.test:
+        print("starting test")
+        words = []
+        # load()
+        d = enchant.Dict("en_US") # GB isn't working, doesn't recognize 'entrancei' as "entrance i"
+
+        with open("./temp2.txt") as f:
+            lines = f.read().splitlines()
+            for i in tqdm(range(len(lines))):
+                line = lines[i]
+
+                corrections = spell_correct(args, d, line.rstrip())
+
+                words.append((line.rstrip(), "".join(corrections)))
+
+        words_str = "\n".join([": ".join(word) for word in words])
+        print(words_str)
+        with open("./unk_words3-fix3.txt", "w") as f:
+            f.write(words_str)
+        print("done")
+        exit(0)
 
     if args.lemma:
         lemmatizer = WordNetLemmatizer()
@@ -71,100 +196,60 @@ def main(args):
     if args.stats:
         suffix = "-stats"
 
-    print("Tokenizing data to", suffix, file=sys.stderr)
 
-    # Define output directory (if not provided)
     if not args.output_dir_base:
-        output_dir = args.corpus_dir.rstrip("/") + suffix
-    else: output_dir = args.output_dir_base.rstrip("/") + suffix
-
+        base = args.corpus_dir if not args.filepath else os.path.dirname(args.filepath)
+        output_dir = base.rstrip("/") + suffix
+    else:
+        output_dir = args.output_dir_base.rstrip("/") + suffix
+    print(output_dir)
     # Create output directory
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
-    for i in tqdm(range(len(files))):
-        file = files[i]
-        output_file = os.path.join(output_dir, os.path.basename(file))
+    print(timestamp() + " Tokenizing data to", suffix, file=sys.stderr)
+
+    d = enchant.Dict("en_US") # GB isn't working, doesn't recognize 'entrancei' as "entrance i"
+
+    # If processing one file, don't loop!
+    if args.filepath:
+        if not os.path.splitext(args.filepath)[1] == ".txt":
+            print(timestamp() + " Must input text file. Exiting...", file=sys.stderr)
+            exit(0)
+        output_file = os.path.join(output_dir, os.path.basename(args.filepath))
         if not args.overwrite and os.path.exists(output_file):
-            continue
+            exit(0)
 
-        output = []
-        with open(file, "r") as f:
-            print("opened:", file)
-            for line in f:
-                if not line.strip():
-                    continue
-
-                # Lower line if needed
-                if args.lower and not args.stats:
-                    line = line.lower()
-
-                # Fix apostrophe escapes:
-                line = line.replace("\\\'", "'")
-
-                # Tokenize line
-                tokens = word_tokenize(line)
-
-                # Join $ to names
-                for i, tok in enumerate(tokens):
-                    if tok == '$':
-                        tokens[i:i+2] = [''.join(tokens[i:i+2])]
-
-                if args.bigrams and not args.stats:
-                    tokens = make_bigrams(tokens)
-                    output.append(" ".join(tokens))
-                    continue
-
-                # Handle issue with dashes appearing at start of word
-                mod_tokens = []
-                for i in range(len(tokens)):
-                    mod_tokens += tokens[i].split() #fix_hyphens(tokens[i])
-                tokens = mod_tokens
-
-
-                if not args.stats:
-                    # First, remove trailing hyphens and slashes
-                    # dash_pattern = r'([^‒–—―\-\\]*)([‒–—―\-\\]+)$' # FIX THIS: ITS REMOVING ALL TRAILING PUNCT
-                    # tokens = [re.sub(dash_pattern, '\\1', x) for x in tokens]
-                    sub_pattern = '\A([\W_]*)([A-Za-z0-9]+|[A-Za-z0-9]+[\W_]+[A-Za-z0-9]+)([\W_]*)$'
-                    tokens = [re.sub(sub_pattern, "\\2", x) for x in tokens]
-                    # Keep all words containing at least one letter
-                    # Also remove words of length < 2
-                    # Lemmatize if necessary
-                    if args.lemma:
-                        tokens = [lemmatizer.lemmatize(x) for x in tokens if len(x) > 2 and re.search('[a-zA-Z]', x)]
-                    else:
-                        tokens = [x for x in tokens if len(x) > 2 and re.search('[a-zA-Z]', x)]
-
-                # Replace split contractions with full words
-                tokens = contractions(tokens)
-
-                # If tokenizing text in order to find useful stats, do extra
-                # processing and return without removing words
-                if args.stats:
-                    output.append(" ".join(tokens))
-                    continue
-
-                finished = " ".join(tokens)
-
-                # If needed, replace street names with generic version
-                if args.street_sub:
-                    finished = re.sub("([^ ]+\-street)|([A-Z][a-z]* street)", "$name_street", finished)
-                    # finished = re.sub("([^ ]+\-lane)|([A-Z][a-z]* lane)", "$name_street", finished)
-                    # finished = re.sub("([^ ]+\-road)|([A-Z][a-z]* road)", "$name_street", finished)
-                    # finished = re.sub("[^ ]+\-row", "$name_street", finished)
-                    # also -square, -highway, -cross, -grove, -town
-
-                output.append(finished)
-
+        # Tokenize single file
+        output = tokenize_file(args, args.filepath, output_dir, d)
+        # Write output to new file
         with open(output_file, "w+") as f:
             f.write('\n'.join(output))
-    print("Tokenization done.", file=sys.stderr)
+        exit(0)
+    else:
+        # Compile list of files to tokenize
+        files = [os.path.join(args.corpus_dir, f) for f in os.listdir(args.corpus_dir)
+                 if (os.path.isfile(os.path.join(args.corpus_dir, f)) and f.endswith('.txt'))]
+
+        for i in tqdm(range(len(files))):
+            file = files[i]
+            # Define path for new tokenized file
+            output_file = os.path.join(output_dir, os.path.basename(file))
+            if not args.overwrite and os.path.exists(output_file):
+                continue
+
+            # Tokenize single file
+            output = tokenize_file(args, file, output_dir, d)
+            # Write output to new file
+            with open(output_file, "w+") as f:
+                f.write('\n'.join(output))
+        print(timestamp() + " Tokenization done.", file=sys.stderr)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--output_dir_base', type=str, default="", help='location to save tokenized text')
     parser.add_argument('--corpus_dir', type=str, default="/work/clambert/thesis-data/sessionsPapers-txt", help='directory containing corpus')
+    parser.add_argument('--filepath', type=str, default="", help='path to single file to be tokenized')
     parser.add_argument('--overwrite', default=False, action="store_true", help='whether or not to overwrite old files with the same names')
     parser.add_argument('--bigrams', default=False, action="store_true", help='whether or not to convert data to bigrams')
     parser.add_argument('--lower', default=False, action="store_true", help='whether or not to lowercase all text')
@@ -172,5 +257,6 @@ if __name__ == '__main__':
     # parser.add_argument('--no_proper_nouns', default=False, action="store_true", help='whether or not to discard all proper nouns')
     parser.add_argument('--street_sub', default=False, action="store_true", help='whether or not to substitute street names with generic string')
     parser.add_argument('--lemma', default=False, action="store_true", help='whether or not to lemmatize all text')
+    parser.add_argument('--test', default=False, action="store_true", help='testing wordninja')
     args = parser.parse_args()
     main(args)
