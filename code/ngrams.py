63ee661a2e27df69b1eb40a101c5a1f6c0b4b6df
Charlotte Lambert <clambert@vassar.edu>
2020-02-28 14:39:52 -0500

    Compute both unigrams and bigrams.

--- code/ngrams.py
+++ code/ngrams.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import nltk, json, os, sys, operator, argparse
+import nltk, json, os, sys, operator, argparse, copy
 from nltk.tokenize import word_tokenize, sent_tokenize
 from tqdm import tqdm
 from utils import *
@@ -15,34 +15,38 @@ def valid_file(filename):
     if year[0] == 1834 and year[1] < 10: return True
     return False
 
-def make_bigram_dict(bigram_dict, text):
+def make_ngram_dicts(unigram_dict, bigram_dict, text):
     words = [word.replace("\\", "") for sent in sent_tokenize(text) for word in word_tokenize(sent)]
 
     words = filter(lambda w: w not in ',-;?.():!', words)
+    uni_words = list(copy.deepcopy(words))
 
     # Create your bigrams
     bgs = nltk.bigrams(words)
-
     # compute frequency distribution for all the bigrams in the text
-    fdist = nltk.FreqDist(bgs)
-    bigram_dict.update({" ".join(k): v for (k,v) in fdist.items()})
-    #for k,v in sorted(fdist.items(), key=operator.itemgetter(1)):
-    #    print(k,v)
+    bigram_dist = nltk.FreqDist(bgs)
+    bigram_dict.update({" ".join(k): v for (k,v) in bigram_dist.items()})
 
-    return bigram_dict
+    # compute frequency distribution for all the unigrams in the text
+    unigram_dist = nltk.FreqDist(uni_words)
+    unigram_dict.update({k: v for (k,v) in unigram_dist.items()})
+    return (unigram_dict, bigram_dict)
 
 def main(args):
-    if args.output_path:
-        out = args.output_path
-    else: out = os.path.join(args.corpus_dir, "corpus_bigrams.json")
+    prefix = os.path.dirname(args.corpus_dir) if args.file else args.corpus_dir
+    uni_out = os.path.join(prefix, "corpus_unigrams.json")
+    bi_out = os.path.join(prefix, "corpus_bigrams.json")
 
-    if os.path.isfile(out) and not args.overwrite:
+    if os.path.isfile(bi_out) and not args.overwrite:
         print("Bigram file already exists. Include overwrite flag to recompute bigrams.", file=sys.stderr)
+        exit(1)
+    if os.path.isfile(uni_out) and not args.overwrite:
+        print("Unigram file already exists. Include overwrite flag to recompute unigrams.", file=sys.stderr)
         exit(1)
 
     if args.file:
         text = open(args.corpus_dir).read()
-        bigram_dict = make_bigram_dict({}, text)
+        unigram_dict, bigram_dict = make_ngram_dicts({}, {}, text)
     else:
         files = [os.path.join(args.corpus_dir, f) for f in os.listdir(args.corpus_dir)
                  if (os.path.isfile(os.path.join(args.corpus_dir, f)) and f.endswith('.txt'))]
@@ -51,26 +55,33 @@ def main(args):
             print(timestamp(),"Filtering input files to all files between 1674 and October 1834...", file=sys.stderr)
             files = [file for file in files if valid_file(file)]
 
-        print(timestamp(),"Computing bigrams...", file=sys.stderr)
+        print(timestamp(),"Computing unigrams and bigrams...", file=sys.stderr)
+        unigram_dict = {}
         bigram_dict = {}
         for i in tqdm(range(len(files))):
             file = files[i]
             text = open(file).read()
-            bigram_dict = make_bigram_dict(bigram_dict, text)
+            unigram_dict, bigram_dict = make_ngram_dicts(unigram_dict, bigram_dict, text)
 
+    unigram_dict = dict(sorted(unigram_dict.items(), key=operator.itemgetter(1)))
     bigram_dict = dict(sorted(bigram_dict.items(), key=operator.itemgetter(1)))
 
     # Write bigram dictionary to output file
-    j = json.dumps(bigram_dict)
-    f = open(out, "w")
-    f.write(j)
+    b = json.dumps(bigram_dict)
+    f = open(bi_out, "w")
+    f.write(b)
     f.close()
-    print(timestamp() + " Wrote bigram dictionary to", out, file=sys.stderr)
+    print(timestamp() + " Wrote bigram dictionary to", bi_out, file=sys.stderr)
+    # Write unigram dictionary to output file
+    u = json.dumps(unigram_dict)
+    f = open(uni_out, "w")
+    f.write(u)
+    f.close()
+    print(timestamp() + " Wrote unigram dictionary to", uni_out, file=sys.stderr)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('corpus_dir', type=str, default="/work/clambert/thesis-data/sessionsAndOrdinarys-txt", help='directory containing corpus')
-    parser.add_argument('--output_path', type=str, default="", help='location to save output bigram dictionary')
     parser.add_argument('--overwrite', default=False, action="store_true", help='whether or not to overwrite old files with the same names')
     parser.add_argument('--file', default=False, action="store_true", help='whether corpus_dir path is a file or not')
     parser.add_argument('--disable_filter', default=False, action="store_true", help='whether or not to disable filtering between 1674 and 1834')
