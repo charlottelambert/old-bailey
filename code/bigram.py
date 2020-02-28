2d0f76de2cc6b1ff64c59f086b70bfdc58d8e506
Charlotte Lambert <clambert@vassar.edu>
2020-02-28 14:21:03 -0500

    Add more helpful prints.

--- code/bigram.py
+++ code/bigram.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import nltk, json, os, sys, operator
+import nltk, json, os, sys, operator, argparse
 from nltk.tokenize import word_tokenize, sent_tokenize
 from tqdm import tqdm
 from utils import *
@@ -31,26 +31,48 @@ def make_bigram_dict(bigram_dict, text):
 
     return bigram_dict
 
+def main(args):
+    if args.output_path:
+        out = args.output_path
+    else: out = os.path.join(args.corpus_dir, "corpus_bigrams.json")
 
-if os.path.isfile(sys.argv[1]):
-    text = open(sys.argv[1]).read()
-    bigram_dict = make_bigram_dict({}, text)
-else:
-    files = [os.path.join(sys.argv[1], f) for f in os.listdir(sys.argv[1])
-             if (os.path.isfile(os.path.join(sys.argv[1], f)) and f.endswith('.txt'))]
-    # Want only 1674 through Oct 1834
-    files = [file for file in files if valid_file(file)]
+    if os.path.isfile(out) and not args.overwrite:
+        print("Bigram file already exists. Include overwrite flag to recompute bigrams.", file=sys.stderr)
+        exit(1)
 
-    bigram_dict = {}
-    for i in tqdm(range(len(files))):
-        file = files[i]
-#    for file in files:
-        text = open(file).read()
-        bigram_dict = make_bigram_dict(bigram_dict, text)
+    if args.file:
+        text = open(args.corpus_dir).read()
+        bigram_dict = make_bigram_dict({}, text)
+    else:
+        files = [os.path.join(args.corpus_dir, f) for f in os.listdir(args.corpus_dir)
+                 if (os.path.isfile(os.path.join(args.corpus_dir, f)) and f.endswith('.txt'))]
+        # Want only 1674 through Oct 1834
+        if not args.disable_filter:
+            print(timestamp(),"Filtering input files to all files between 1674 and October 1834...", file=sys.stderr)
+            files = [file for file in files if valid_file(file)]
 
-bigram_dict = dict(sorted(bigram_dict.items(), key=operator.itemgetter(1)))
+        print(timestamp(),"Computing bigrams...", file=sys.stderr)
+        bigram_dict = {}
+        for i in tqdm(range(len(files))):
+            file = files[i]
+            text = open(file).read()
+            bigram_dict = make_bigram_dict(bigram_dict, text)
 
-json = json.dumps(bigram_dict)
-f = open("/work/clambert/thesis-data/corpus_bigrams.json", "w")
-f.write(json)
-f.close()
+    bigram_dict = dict(sorted(bigram_dict.items(), key=operator.itemgetter(1)))
+
+    # Write bigram dictionary to output file
+    j = json.dumps(bigram_dict)
+    f = open(out, "w")
+    f.write(j)
+    f.close()
+    print(timestamp() + " Wrote bigram dictionary to", out, file=sys.stderr)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('corpus_dir', type=str, default="/work/clambert/thesis-data/sessionsAndOrdinarys-txt", help='directory containing corpus')
+    parser.add_argument('--output_path', type=str, default="", help='location to save output bigram dictionary')
+    parser.add_argument('--overwrite', default=False, action="store_true", help='whether or not to overwrite old files with the same names')
+    parser.add_argument('--file', default=False, action="store_true", help='whether corpus_dir path is a file or not')
+    parser.add_argument('--disable_filter', default=False, action="store_true", help='whether or not to disable filtering between 1674 and 1834')
+    args = parser.parse_args()
+    main(args)
