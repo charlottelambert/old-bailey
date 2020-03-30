9faf4ab8aed44176ea41ba07136c715c8b32ba77
Charlotte Lambert <clambert@vassar.edu>
2020-03-30 19:23:51 -0400

    Make coherence work for unigrams.

--- topic-modeling/coherence.py
+++ topic-modeling/coherence.py
@@ -16,6 +16,7 @@ def coherence(T, corpus):
         input:
             T (list of strings): topic to find coherence of
     """
+
     sum = 0
     # Go through all distinct pairs in the topic
     for i, item_i in enumerate(T):
@@ -26,9 +27,15 @@ def coherence(T, corpus):
             # Calculate how many documents w_i appears in and how many
             # documents w_i and w_j both appear in
             counts = D(corpus, w_i, second_word=w_j)
-            val = (counts["both"] + 1) / counts[w_i]
             # Update sum
-            sum += math.log(val, 10)
+            try:
+                val = (counts["both"] + 1) / counts[w_i]
+                sum += math.log(val, 10)
+            except ZeroDivisionError:
+                # print(val)
+                print(counts)
+                print(w_j)
+                continue
     return sum
 
 def load_docs(args):
@@ -40,12 +47,14 @@ def D(corpus, word, second_word=None):
     counts = {word:0}
     if second_word:
         counts["both"] = 0
-    for doc_name, doc in corpus.docs.items():
+    # Need corpus to be in format: {filename: file_contents.split()}
+    for doc_name, doc in corpus.items():
         # Update D(w_i)
-        if word in doc.text():
+        text = doc
+        if word in text:
             counts[word] += 1
             # Update D(w_i, w_j)
-            if second_word and second_word in doc.text():
+            if second_word and second_word in text:
                 counts["both"] += 1
     return counts
 
@@ -53,6 +62,9 @@ def main(args):
     # Extract topics from each model
     # export PYTHONPATH=/home/clambert/thesis/topic-modeling/lda-tools/lib:$PYTHONPATH
     corpus = load_docs(args)
+    new_corpus = {}
+    for file, doc in corpus.docs.items():
+        new_corpus[file] = set(doc.text().lower().split())
     # print(D(corpus, "murther", second_word="murder"))
     # NOW IMPLEMENT THAT MASS THING!!
     # exit(0)
@@ -63,8 +75,10 @@ def main(args):
     topics = read_weighted_keys(weighted_keys)
     print("Done!", file=sys.stderr)
     print("Calculating coherence for", len(topics) ,"topics...", file=sys.stderr)
+    c_value = 0
     for id, topic in tqdm(topics.items()):
-        print(coherence(topic, corpus))
+        c_value += coherence(topic, new_corpus)
+    print("Average topic coherence:", c_value / len(topics))
     print("Done!", file=sys.stderr)
 
 
