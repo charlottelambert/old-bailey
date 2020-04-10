9a96762085fe7695957f831638fdd66803d15764
Charlotte Lambert <clambert@vassar.edu>
2020-04-09 22:48:34 -0400

    Fix proper noun count.

--- analyze/analyze_utils.py
+++ analyze/analyze_utils.py
@@ -20,7 +20,7 @@ def parse_iterate(file, noun_counts=False, freq_dist=False):
         if freq_dist: word_list.append(word)
         if noun_counts:
             # If word is a noun, increment appropriate count
-            if pos == 'NNP': proper_nouns += 1
+            if pos == 'NNP' or pos == "NNPS": proper_nouns += 1
             elif pos[0] == 'N': other_nouns += 1
     ret = []
     if noun_counts:
@@ -34,25 +34,6 @@ def noun_counts(file):
         File must be in POS parsed format.
     """
     return parse_iterate(file, noun_counts=True)
-    # proper_nouns = 0
-    # other_nouns = 0
-    #
-    # # Open input file and extract content
-    # with open(file, 'r') as f:
-    #     content = f.read()
-    # matches = re.findall(r"\([A-Z]+ [a-z]+\)", content)
-    # total_words = len(matches)
-    #
-    # # Iterate over all words in corpus
-    # for match in matches:
-    #     match = match[1:-1]
-    #     pos, word = match.split()
-    #
-    #     # If word is a noun, increment appropriate count
-    #     if pos == 'NNP': proper_nouns += 1
-    #     elif pos[0] == 'N': other_nouns += 1
-    #
-    # return proper_nouns, other_nouns, total_words
 
 def bnc_fd(files):
     fd = nltk.FreqDist()
