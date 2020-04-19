3fb266cf84672be3d2d1ee1edd5a3276477832a7
Charlotte Lambert <clambert@vassar.edu>
2020-04-19 15:29:37 -0400

    OB + LL bigrams, TSV.

--- data/prep_tsv.py
+++ data/prep_tsv.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import sys, optparse, os,shutil
 import numpy as np
+sys.path.append("..")
+from utils import timestamp
 
 def main(options, args):
     if len(args) > 0:
-        print("Merging TSV files...", file=sys.stderr)
+        print(timestamp(), "Merging TSV files...", file=sys.stderr)
         base = os.path.dirname(args[0])
         suffix = os.path.basename(args[0]).split("-")[2:]
 
         output_file = base.replace(".tsv-dir", "-" + "-".join(suffix))
-        print("Merged tokenized TSV file being written to", output_file)
+        print(timestamp(), "Merged tokenized TSV file being written to", output_file)
         all_lines = []
         for file in args:
             if "split-" not in os.path.basename(file): continue
@@ -21,11 +23,11 @@ def main(options, args):
         with open(output_file,'w') as f:
             f.write("\n".join(all_lines))
         if options.rm_dir: shutil.rmtree(base)
-        print("Done!", file=sys.stderr)
+        print(timestamp(), "Done!", file=sys.stderr)
 
 
     elif options.tsv_file:
-        print("Splitting TSV file...", file=sys.stderr)
+        print(timestamp(), "Splitting TSV file...", file=sys.stderr)
         with open(options.tsv_file, 'r') as f:
             lines = f.read().split("\n")
             # header = lines[0]
@@ -38,7 +40,7 @@ def main(options, args):
             try:
                 assert sum([len(l) for l in splits]) == len(lines[1:])
             except AssertionError:
-                print("Lost some lines, not sure why. Exiting...", file=sys.stderr)
+                print(timestamp(), "Lost some lines, not sure why. Exiting...", file=sys.stderr)
 
             for i, s in enumerate(splits):
                 # Unique file path
@@ -49,7 +51,7 @@ def main(options, args):
                 with open(out_file, 'w') as f:
                     # f.write("\n".join([header] + s))
                     f.write("\n".join(s))
-        print("Done!")
+        print(timestamp(), "Done!")
     else:
         print("Input files to merge or file to split.", file=sys.stderr)
         exit(0)
