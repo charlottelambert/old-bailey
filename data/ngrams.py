104ec6f38f57a2ff2ce7e9cbaacd004812225e94
Charlotte Lambert <clambert@vassar.edu>
2020-05-20 02:17:30 -0400

    Document ngram code.

--- data/ngrams.py
+++ data/ngrams.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 sys.path.append('../')
 from utils import *
 
+# List of stopwords to excldue from text
 stop_words = set(stopwords.words('english'))
 
 def valid_file(filename):
@@ -22,13 +23,19 @@ def valid_file(filename):
     return False
 
 def make_ngram_dicts(unigram_dict, bigram_dict, text):
+    """
+        Make dictionaries containing unigrams and bigrams given text.
+    """
+    # Get words excluding stopwords
     words = [word.replace("\\", "") for sent in sent_tokenize(text) for word in word_tokenize(sent) if word not in stop_words]
 
+    # Filter out punctuation and collect unigrams
     words = filter(lambda w: w not in ',-;?.():!', words)
     uni_words = list(copy.deepcopy(words))
 
     # Create your bigrams
     bgs = nltk.bigrams(words)
+
     # compute frequency distribution for all the bigrams in the text
     bigram_dist = nltk.FreqDist(bgs)
     bigram_dict.update({" ".join(k): v for (k,v) in bigram_dist.items()})
@@ -39,10 +46,12 @@ def make_ngram_dicts(unigram_dict, bigram_dict, text):
     return (unigram_dict, bigram_dict)
 
 def main(args):
+    # Generate filenames for saving ngram json files
     prefix = os.path.dirname(args.corpus_dir) if args.file else args.corpus_dir
     uni_out = os.path.join(prefix, "corpus_unigrams.json")
     bi_out = os.path.join(prefix, "corpus_bigrams.json")
-    print(bi_out)
+
+    # Exit if files exist and overwrite flag is false
     if os.path.isfile(bi_out) and not args.overwrite:
         print("Bigram file already exists. Include overwrite flag to recompute bigrams.", file=sys.stderr)
         exit(1)
@@ -50,12 +59,15 @@ def main(args):
         print("Unigram file already exists. Include overwrite flag to recompute unigrams.", file=sys.stderr)
         exit(1)
 
+    # If input is file, make the dictionaries just from the text in the file
     if args.file:
         text = open(args.corpus_dir).read()
         unigram_dict, bigram_dict = make_ngram_dicts({}, {}, text)
+    # If in put is directory, make dictionaries from text in all files
     else:
-        files = [os.path.join(args.corpus_dir, f) for f in os.listdir(args.corpus_dir)
-                 if (os.path.isfile(os.path.join(args.corpus_dir, f)) and f.endswith('.txt'))]
+        # List of valid files (make sure to exclude json files)
+        files_dict, _ = order_files(args)
+        files = list(files_dict.values())[0]
         # Want only 1674 through Oct 1834
         if not args.disable_filter:
             print(timestamp(),"Filtering input files to all files between 1674 and October 1834...", file=sys.stderr)
@@ -64,26 +76,29 @@ def main(args):
         print(timestamp(),"Computing unigrams and bigrams...", file=sys.stderr)
         unigram_dict = {}
         bigram_dict = {}
-        for i in tqdm(range(len(files))):
-            file = files[i]
+        # Iterate over each file and add unigram and bigram counts to
+        # dictionaries
+        for file in tqdm(files):
             text = open(file).read()
             unigram_dict, bigram_dict = make_ngram_dicts(unigram_dict, bigram_dict, text)
 
+    # Sort dictionaries in order of most common ngrams
     unigram_dict = dict(sorted(unigram_dict.items(), key=operator.itemgetter(1)))
     bigram_dict = dict(sorted(bigram_dict.items(), key=operator.itemgetter(1)))
 
     # Write bigram dictionary to output file
     b = json.dumps(bigram_dict)
-    f = open(bi_out, "w")
-    f.write(b)
-    f.close()
+    with open(bi_out, "w") as f:
+        f.write(b)
     print(timestamp() + " Wrote bigram dictionary to", bi_out, file=sys.stderr)
+
     # Write unigram dictionary to output file
     u = json.dumps(unigram_dict)
-    f = open(uni_out, "w")
-    f.write(u)
-    f.close()
+    with open(uni_out, "w") as f:
+        f.write(u)
     print(timestamp() + " Wrote unigram dictionary to", uni_out, file=sys.stderr)
+
+    # Write unigram personal word list to file
     pwl_out = os.path.join(prefix, "unigram_pwl.txt")
     with open(pwl_out, "w") as f:
         f.write("\n".join(word for word, freq in unigram_dict.items()))
@@ -92,7 +107,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('corpus_dir', type=str, default="/work/clambert/thesis-data/sessionsAndOrdinarys-txt", help='directory containing corpus')
+    parser.add_argument('corpus_dir', type=str, default="/work/clambert/thesis-data/OB_LL-txt", help='directory containing corpus')
     parser.add_argument('--overwrite', default=False, action="store_true", help='whether or not to overwrite old files with the same names')
     parser.add_argument('--file', default=False, action="store_true", help='whether corpus_dir path is a file or not')
     parser.add_argument('--disable_filter', default=False, action="store_true", help='whether or not to disable filtering between 1674 and 1834')
